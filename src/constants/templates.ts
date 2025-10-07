6e70accf9f97337855209f769551ba0772a1c440
TaDangQuang <Tadangquang2005@gmail.com>
2025-10-07 15:02:28 +0700

    Complete collaboration setup

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+    { 
+    id: "blank", 
+    label: "Blank Document", 
+    imageUrl: "/blank-document.svg",
+    },
+    { 
+    id: "software-proposal", 
+    label: "Software development proposal", 
+    imageUrl: "/software-proposal.svg",
+    },
+    { 
+    id: "project-proposal", 
+    label: "Project proposal", 
+    imageUrl: "/project-proposal.svg",
+    },
+    { 
+    id: "business-letter", 
+    label: "Business letter", 
+    imageUrl: "/business-letter.svg",
+    },
+    { 
+    id: "cover-letterr", 
+    label: "Cover letter", 
+    imageUrl: "/cover-letter.svg",
+    },
+    { 
+    id: "resume", 
+    label: "Resume", 
+    imageUrl: "/resume.svg",
+    },
+    { 
+    id: "letterr", 
+    label: "Letter", 
+    imageUrl: "/letter.svg",
+    },
+];
