e1c4c6ab455b808f7e059029d9872c8a7149c782
TaDangQuang <Tadangquang2005@gmail.com>
2025-10-11 20:41:51 +0700

    12: fixed-3

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -38,7 +38,7 @@ export const TemplatesGallery = () => {
     return (
         <div className="bg-[#F1F3F4]">
             <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
-                <h3 className="font-medium">Bắt đầu với tài liệu mới</h3>
+                <h3 className="font-medium">Start a new document</h3>
                 <Carousel>
                     <CarouselContent className="ml-4">
                         {templates.map((template) => (
